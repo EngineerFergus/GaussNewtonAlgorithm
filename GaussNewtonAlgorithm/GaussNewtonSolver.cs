fa2cce5a65cb82c63304cd35c9c573d708f90cf5
williamskei97 <williamskei97@gmail.com>
2021-11-16 22:46:53 -0500

    Began outlining GaussNewtonSolver. Added SigmoidFunction to Utils for testing.

--- GaussNewtonAlgorithm/GaussNewtonSolver.cs
+++ GaussNewtonAlgorithm/GaussNewtonSolver.cs
@@ -8,9 +8,27 @@ namespace GaussNewtonAlgorithm
 {
     public class GaussNewtonSolver
     {
-        private Func<double, double[], double> fitFunction;
-        private double tolerance;
+        private double rmseTolerance;
+        private double iterationTolerance;
         private int maxIterations;
+
+        public Func<double, double[], double> FitFunction { get; private set; }
         public double[] Coefficients { get; private set; }
+        public Data[] Data { get; private set; }
+
+        public GaussNewtonSolver(Func<double, double[], double> fitFunction, Data[] data, double[] initGuesses,
+            double rmseTol = 10e-9, double iterTol = 10e-16)
+        {
+            rmseTolerance = rmseTol;
+            iterationTolerance = iterTol;
+            FitFunction = fitFunction;
+            Data = data;
+            Coefficients = initGuesses;
+        }
+
+        public void Fit()
+        {
+            // TODO run algo to fit the function
+        }
     }
 }
--- GaussNewtonAlgorithm/Utils.cs
+++ GaussNewtonAlgorithm/Utils.cs
@@ -8,7 +8,17 @@ namespace GaussNewtonAlgorithm
 {
     public static class Utils
     {
+        public static double SigmoidFunction(double x, double[] coefficients)
+        {
+            if(coefficients.Length != 3)
+            {
+                throw new Exception($"Exeption running SigmoidFunction." +
+                    $" Expected three coefficient values and received {coefficients.Length}");
+            }
 
+            double A = coefficients[0], B = coefficients[1], C = coefficients[2];
+            return A / (1 + Math.Exp(-B * (x - C)));
+        }
     }
 
     public record Data(double X, double Y);
