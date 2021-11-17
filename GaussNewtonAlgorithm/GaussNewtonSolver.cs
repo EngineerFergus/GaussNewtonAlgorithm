3b69220f0c1ab299bc383d0f9491e55a79fd7446
williamskei97 <williamskei97@gmail.com>
2021-11-16 22:27:16 -0500

    Added GaussNewtonSolver class and Utils class

---
+++ GaussNewtonAlgorithm/GaussNewtonSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussNewtonAlgorithm
+{
+    public class GaussNewtonSolver
+    {
+        private Func<double, double[], double> fitFunction;
+        private double tolerance;
+        private int maxIterations;
+        public double[] Coefficients { get; private set; }
+    }
+}
---
+++ GaussNewtonAlgorithm/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussNewtonAlgorithm
+{
+    public static class Utils
+    {
+
+    }
+
+    public record Data(double X, double Y);
+}
