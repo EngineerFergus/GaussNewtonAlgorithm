c7bf8b7be02083bb0acc97582710b7ed4673c17e
williamskei97 <williamskei97@gmail.com>
2021-11-21 18:00:04 -0500

    Wrote out data for viewing training.

--- GaussNewtonAlgorithm/Program.cs
+++ GaussNewtonAlgorithm/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 
 namespace GaussNewtonAlgorithm
 {
@@ -9,7 +12,7 @@ namespace GaussNewtonAlgorithm
             Func<double, DMatrix, double> sigmoidFunc = Utils.SigmoidFunction;
             DMatrix trueBeta = DMatrix.ColVector(new double[] { 20, 1, 5 });
 
-            Data[] noisyData = Utils.GenerateNoisyData(sigmoidFunc, trueBeta, -3, 20, 0.15, 100);
+            Data[] noisyData = Utils.GenerateNoisyData(sigmoidFunc, trueBeta, -3, 20, 1.5, 100);
 
             GaussNewtonSolver solver = new GaussNewtonSolver(sigmoidFunc, 100);
 
@@ -21,6 +24,29 @@ namespace GaussNewtonAlgorithm
             Console.WriteLine($"");
             Console.WriteLine($"Beta used to make data: {trueBeta}");
             Console.WriteLine($"BetaHat: {betaHat}");
+            WriteData(solver, noisyData, "SigmoidFit");
+
+            foreach(Data d in noisyData)
+            {
+                Console.WriteLine($"{d.X:F4},{d.Y:F4}");
+            }
+        }
+
+        public static void WriteData(GaussNewtonSolver solver, Data[] data, string experimentName)
+        {
+            //string directory = $"DIRECTORY GOES HERE";
+            //string dataDir = $"{directory}{experimentName}_data.csv";
+            //string fitDir = $"{directory}{experimentName}_trainingSummary.csv";
+            //
+            //StringBuilder d = new StringBuilder();
+            //
+            //foreach(Data x in data)
+            //{
+            //    d.AppendLine($"{x.X},{x.Y}");
+            //}
+            //
+            //File.AppendAllText(dataDir, d.ToString());
+            //File.AppendAllText(fitDir, solver.TrainingInfo.ToString());
         }
     }
 }
